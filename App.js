10795190b5cca8297a4b803cf2987b35ed06da31
gautamkshah <109517113+gautamkshah@users.noreply.github.com>
2024-07-25 02:03:15 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View className="bg-orange-500 flex-1">
+      <Text  >Appss</Text>
+    </View>
+  )
+}
+
+export default App
